08cab2ca15dc468928491fd8181882a657d1a4f2
Edwin Cardenas <eacardenase@pop-os.localdomain>
2022-09-01 11:35:13 -0500

    initial version

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const matchesData = fs
+    .readFileSync('football.csv', { encoding: 'utf-8' })
+    .split('\n')
+    .map((row: string): string[] => row.split(','));
+
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
+let manUnitedWins = 0;
+
+for (let match of matchesData) {
+    if (match[1] === 'Man United' && match[5] === homeWin) {
+        manUnitedWins++;
+    }
+
+    if (match[2] === 'Man United' && match[5] === awayWin) {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
